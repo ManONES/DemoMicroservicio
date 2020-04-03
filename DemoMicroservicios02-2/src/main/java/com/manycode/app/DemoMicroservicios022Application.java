a963948c026a69f79c7ce5cdbbde71fca3cd10b4
SIES <SIES@DESKTOP-DVEVJTF>
2020-04-03 17:00:52 -0600

    1er carga de microservicios

---
+++ DemoMicroservicios02-2/src/main/java/com/manycode/app/DemoMicroservicios022Application.java
@@ -0,0 +1,13 @@
+package com.manycode.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMicroservicios022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMicroservicios022Application.class, args);
+	}
+
+}
